3f1c122c71b03743751b91ff4744ee57b1b44152
xin1126 <z26319491z@gmail.com>
2022-10-14 11:20:21 +0800

    feat: 新增Sortablejs拖曳表格

---
+++ src/components/Sortable.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>Element UI + Sortablejs表格拖曳</h1>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%"
+      :row-class-name="tableRowClassName"
+    >
+      <el-table-column
+        v-for="(value, key) of tableData[0]"
+        :key="key"
+        :prop="key"
+        :label="labelText[key]"
+        align="center"
+      />
+    </el-table>
+  </div>
+</template>
+
+<script>
+import Sortable from "sortablejs";
+export default {
+  props: ["tableData", "labelText", "tableRowClassName"],
+  methods: {
+    rowDrop() {
+      const tbody = document.querySelector(".el-table__body-wrapper tbody");
+      Sortable.create(tbody);
+    }
+  },
+  mounted() {
+    this.rowDrop();
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <Sortable
+      :tableData="tableData"
+      :labelText="labelText"
+      :tableRowClassName="tableRowClassName"
+    />
+  </div>
+</template>
+
+<script>
+import Sortable from "@/components/Sortable.vue";
+export default {
+  components: {
+    Sortable
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          name: "小美",
+          age: "15",
+          gender: "女性",
+          address: "台北市"
+        },
+        {
+          name: "小王",
+          age: "30",
+          gender: "男性",
+          address: "新竹市"
+        },
+        {
+          name: "小花",
+          age: "26",
+          gender: "女性",
+          address: "高雄市"
+        },
+        {
+          name: "小明",
+          age: "17",
+          gender: "男性",
+          address: "新北市"
+        }
+      ],
+      labelText: {
+        name: "名稱",
+        age: "年齡",
+        gender: "性別",
+        address: "地址"
+      }
+    };
+  },
+  methods: {
+    tableRowClassName({ row, rowIndex }) {
+      console.log(row);
+      if (rowIndex === 1) return "warning-row";
+      if (rowIndex === 3) return "success-row";
+      return "";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.home {
+  width: 800px;
+  margin: 0 auto;
+}
+
+.el-table .warning-row {
+  background: oldlace;
+}
+
+.el-table .success-row {
+  background: #f0f9eb;
+}
+
+.el-table__row {
+  cursor: pointer;
+}
+</style>
